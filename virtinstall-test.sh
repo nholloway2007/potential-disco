593dd01f86027b73bf84edfa5919df62be4e573f
TheGreenHundred <thegreenhundred@gmail.com>
2017-07-20 10:18:53 -0500

    update for readability

--- virtinstall-test.sh
+++ virtinstall-test.sh
@@ -8,4 +8,14 @@ read -p "OS Type: " TYPE
 read -p "OS Variant: " VARIANT
 read -p "Image location: " LOCATION
 
-sudo virt-install --name $NAME --ram $RAM --disk-path $PATH --vcpus $VCPU --os-type $TYPE --os-variant $VARIANT --graphics none --console pty,target_type=serial --location $LOCATION --extra-args 'console=ttyS0,115200n8 serial'
+sudo virt-install \
+--name $NAME \
+--ram $RAM \
+--disk-path $PATH \
+--vcpus $VCPU \
+--os-type $TYPE \
+--os-variant $VARIANT \
+--graphics none \
+--console pty,target_type=serial \
+--location $LOCATION \
+--extra-args 'console=ttyS0,115200n8 serial'
