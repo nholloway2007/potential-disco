2d4518b8f22ccf250aa917bfba4b7e790b86c910
Nickolas <nholloway2007@gmail.com>
2017-07-20 10:13:19 -0500

    virtinstall

---
+++ virtinstall-test.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+read -p "Virtual Machine Name: " NAME
+read -p "Allocated RAN: " RAM
+read -p "Storage Path: " PATH
+read -p "Virtual CPUs: " VCPU
+read -p "OS Type: " TYPE
+read -p "OS Variant: " VARIANT
+read -p "Image location: " LOCATION
+
+sudo virt-install --name $NAME --ram $RAM --disk-path $PATH --vcpus $VCPU --os-type $TYPE --os-variant $VARIANT --graphics none --console pty,target_type=serial --location $LOCATION --extra-args 'console=ttyS0,115200n8 serial'
